bb96873cf2ecf40639bee564fe479e647c836e54
leeaction <leeaction@163.com>
2015-06-04 19:55:07 +0800

    test commit4  feature branch 333

--- Test_Git_new/src/TestClass.java
+++ Test_Git_new/src/TestClass.java
@@ -6,6 +6,12 @@ public class TestClass {
 	 */
 	public static void main(String[] args) {
 		System.out.println("test git");
+		
+		System.out.println("dev on feature branch...");
+		
+		System.out.println("dev on feature branch 222...");
+		
+		System.out.println("dev on feature branch 333...");
 
 	}
 
