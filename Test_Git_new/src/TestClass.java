0396772e3315cc873015c9187dfebacd76e9a982
leeaction <leeaction@163.com>
2015-06-04 19:55:07 +0800

    test commit4  feature branch 333

--- Test_Git_new/src/TestClass.java
+++ Test_Git_new/src/TestClass.java
@@ -5,7 +5,13 @@ public class TestClass {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("test git");
+		
+		System.out.println("dev on feature branch...");
+		
+		System.out.println("dev on feature branch 222...");
+		
+		System.out.println("dev on feature branch 333...");
 
 	}
 
